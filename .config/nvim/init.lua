52631611bade8982056a7d0615d8fbca4e1bbdc1
Eric Crowder <ebcrowder@gmail.com>
2022-11-19 20:13:47 -0800

    Only run eslint when package.json exists

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -242,6 +242,9 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint.with({
       prefer_local = "node_modules/.bin",
+      condition = function(utils)
+        return utils.root_has_file({ "package.json" })
+      end,
     }),
     null_ls.builtins.formatting.prettier.with({
       prefer_local = "node_modules/.bin",
